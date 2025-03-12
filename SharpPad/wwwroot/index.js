ac505ee3f942122920f8ddc762b7d35f6c42dc76
conggaof <conggaof@isoftstone.com>
2025-03-12 18:07:56 +0800

    feat: 更新 Monaco Editor 加载路径为 CDN 资源
    - 修改 index.js 中的 Monaco Editor 加载路径为 unpkg.com CDN
    - 更新 vs 路径和 loader.js 脚本为指定版本 0.52.2

--- SharpPad/wwwroot/index.js
+++ SharpPad/wwwroot/index.js
@@ -1,7 +1,7 @@
 // Monaco Editor 配置
 const monacoConfig = {
     paths: {
-        'vs': 'monaco-editor/min/vs'
+        'vs': 'https://unpkg.com/monaco-editor@0.52.2/min/vs'
     }
 };
 
@@ -9,7 +9,7 @@ const monacoConfig = {
 function loadMonaco() {
     return new Promise((resolve) => {
         const script = document.createElement('script');
-        script.src = 'monaco-editor/min/vs/loader.js';
+        script.src = 'https://unpkg.com/monaco-editor@0.52.2/min/vs/loader.js';
         script.onload = () => {
             require.config(monacoConfig);
             require(['vs/editor/editor.main'], () => {
