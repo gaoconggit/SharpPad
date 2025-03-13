0c6d43636ee29d2efa176f08b1d61bf604e186e2
conggaof <conggaof@isoftstone.com>
2025-03-13 16:39:52 +0800

    feat: 更新默认代码示例为异步逐字符打印效果
    - 修改 common.js 中的 DEFAULT_CODE，重写 Main 方法为异步方法
    - 使用 await Task.Delay 替代 Thread.Sleep 实现字符间的延时效果，增强代码示例的交互性

--- SharpPad/wwwroot/utils/common.js
+++ SharpPad/wwwroot/utils/common.js
@@ -62,19 +62,19 @@ export function showNotification(message, type = 'info') {
 
 //定义常量
 export const DEFAULT_CODE = `using System;
-using System.Threading;
+using System.Threading.Tasks;
 
 class Program
 {
-    static void Main()
+    public static async Task Main()
     {
         string message = "Hello, SharpPad! 关注我: https://github.com/gaoconggit/SharpPad";
         foreach (char c in message)
         {
-            Console.Write(c);  // 使用Console.Write逐字符打印
-            Thread.Sleep(100); // 设置适当的延时，单位是毫秒
+            Console.Write(c);
+            await Task.Delay(100);
         }
-        Console.WriteLine();  // 打印结束后换行
+        Console.WriteLine();
     }
 }`;
 
